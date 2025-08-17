253a9a4a9f9941537f5446ef9c08a8f304e9314d
Abhinav Kumar Singh <152812718+abhinavkr26104@users.noreply.github.com>
2025-08-17 13:51:15 +0530

    Time: 3 ms (100%), Space: 42.8 MB (99.52%) - LeetHub

--- 0867-new-21-game/0867-new-21-game.java
+++ 0867-new-21-game/0867-new-21-game.java
@@ -1,15 +1,18 @@
 class Solution {
     public double new21Game(int n, int k, int maxPts) {
         if (k == 0 || n >= k + maxPts - 1) return 1.0;
-        double[] dp = new double[n + 1];
-        dp[0] = 1.0;
-        double w = 1.0, ans = 0.0;
-        for (int i = 1; i <= n; i++) {
-            dp[i] = w / maxPts;
-            if (i < k) w += dp[i];
-            if (i - maxPts >= 0 && i - maxPts < k) w -= dp[i - maxPts];
+        double[] dp = new double[k + 1];
+        dp[k] = 1.0;
+        double window = Math.min(n - k + 1, maxPts);
+        for (int i = k - 1; i >= 0; --i) {
+            dp[i] = window / maxPts;
+            window += dp[i];
+            double out;
+            int j = i + maxPts;
+            if (j > k) out = (j <= n) ? 1.0 : 0.0;
+            else out = dp[j];
+            window -= out;
         }
-        for (int i = k; i <= n; i++) ans += dp[i];
-        return ans;
+        return dp[0];
     }
 }
