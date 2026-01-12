e0e2d8d2810740689b6cf885958eac18f56cb539
Abhinav Kumar Singh <152812718+abhinavkr26104@users.noreply.github.com>
2026-01-12 15:02:29 +0530

    Time: 1 ms (99.58%), Space: 48.6 MB (93.78%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,43 +1,43 @@
 class Solution {
-      static {
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            try (FileWriter writer = new FileWriter("display_runtime.txt")) {
-                writer.write("0");
-            } catch (IOException e) {
-                e.printStackTrace();
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        if (nums1.length > nums2.length) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+        int m = nums1.length, n = nums2.length;
+        int left = 0, right = m;
+        
+        while (left <= right) {
+            int partitionX = (left + right) / 2;
+            int partitionY = (m + n + 1) / 2 - partitionX;
+            
+            int maxLeftX = (partitionX == 0) ? Integer.MIN_VALUE : nums1[partitionX - 1];
+            int minRightX = (partitionX == m) ? Integer.MAX_VALUE : nums1[partitionX];
+            
+            int maxLeftY = (partitionY == 0) ? Integer.MIN_VALUE : nums2[partitionY - 1];
+            int minRightY = (partitionY == n) ? Integer.MAX_VALUE : nums2[partitionY];
+            
+            if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
+                if ((m + n) % 2 == 0) {
+                    return (Math.max(maxLeftX, maxLeftY) + Math.min(minRightX, minRightY)) / 2.0;
+                } else {
+                    return Math.max(maxLeftX, maxLeftY);
+                }
+            } else if (maxLeftX > minRightY) {
+                right = partitionX - 1;
+            } else {
+                left = partitionX + 1;
             }
-        }));
+        }
+        return 0.0;  
     }
+}
 
-    public double findMedianSortedArrays(int[] a, int[] b) {
-
-        int i=0;
-        int j=0;
-        int k=0;
-        int c=a.length+b.length;
-        int r[]=new int[c];
-        while(i<a.length && j<b.length){
-            if(a[i]>b[j]){
-             r[k]=b[j];
-             j++;
-            }
-            else{
-                r[k]=a[i];
-                i++;
-            }
-            k++;
-        }
-        while(i<a.length){
-            r[k]=a[i];
-            i++;
-            k++;
-        }
-        while(j<b.length){
-            r[k]=b[j];
-            j++;
-            k++;
-        }
-        if(c%2!=0) return r[c/2];
-        return (r[c/2-1]+r[c/2])/2.0;
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.printf("%.5f\n", sol.findMedianSortedArrays(new int[]{1,3}, new int[]{2}));      // 2.00000
+        System.out.printf("%.5f\n", sol.findMedianSortedArrays(new int[]{1,2}, new int[]{3,4}));  // 2.50000
+        System.out.printf("%.5f\n", sol.findMedianSortedArrays(new int[]{0}, new int[]{0}));      // 0.00000
+        System.out.printf("%.5f\n", sol.findMedianSortedArrays(new int[]{9,9}, new int[]{9}));    // 9.00000
     }
 }
