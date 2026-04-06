9531d38b6d2715e8600f0b165198985e6533d731
Abhinav Kumar Singh <152812718+abhinavkr26104@users.noreply.github.com>
2026-04-07 00:54:23 +0530

    Time: 26 ms (6.51%), Space: 80.9 MB (74.32%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        
+        for (int num : nums) {
+            set.add(num);
+        }
+        
+        return set.size() < nums.length;
+    }
+}
