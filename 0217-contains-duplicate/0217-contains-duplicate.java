bcd1b88da32a96e0655a1aaa6807760443df1922
Abhinav Kumar Singh <152812718+abhinavkr26104@users.noreply.github.com>
2026-04-07 00:58:42 +0530

    Time: 2 ms (100%), Space: 76.6 MB (91.74%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,14 +1,46 @@
-import java.util.HashSet;
-import java.util.Set;
-
 class Solution {
-    public boolean containsDuplicate(int[] nums) {
-        Set<Integer> seen = new HashSet<>(nums.length);
+    //  public boolean containsDuplicate(int[] nums) {
+    //     if (nums == null || nums.length == 0) {
+    //         return false;
+    //     }
+    //     Set<Integer> set = new HashSet<>();
+    //     for (int num : nums) {
+    //         if (set.contains(num)) {
+    //             return true;
+    //         }
+    //         set.add(num);
+    //     }
+    //     return false;
+    // }
+   public boolean  containsDuplicate(int[] arr) {
 
-        for (int num : nums) {
-            if (!seen.add(num)) return true;
+        // Duyệt từ phần tử thứ 2 (index = 1)
+        // Vì phần tử đầu tiên (index = 0) coi như đã sorted
+        for (int i = 1; i < arr.length; i++) {
+
+            // key là phần tử cần chèn vào đúng vị trí
+            int key = arr[i];
+
+            // j dùng để duyệt phần đã sắp xếp (bên trái)
+            int j = i - 1;
+
+            // Dịch các phần tử lớn hơn key sang phải
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j]; // dịch phần tử sang phải
+                j--; // lùi lại để tiếp tục so sánh
+
+            }
+
+if( j >= 0 && key == arr[j] ) return true;
+            // Sau khi tìm được vị trí phù hợp
+            // chèn key vào
+            arr[j + 1] = key;
         }
+        return false ;
+}
+    
+    
 
-        return false;
-    }
+
+
 }
