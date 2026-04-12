6cfb781d07a46a64335518dad3afad2db47cd359
Abhinav Kumar Singh <152812718+abhinavkr26104@users.noreply.github.com>
2026-04-12 18:53:40 +0530

    Time: 2 ms (100%), Space: 46 MB (84.97%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int[] tails = new int[nums.length];
+        int size = 0;
+
+        for (int x : nums) {
+            int i = Arrays.binarySearch(tails, 0, size, x);
+
+            if (i < 0) i = -(i + 1); // insertion point
+
+            tails[i] = x;
+
+            if (i == size) size++;
+        }
+
+        return size;
+    }
+}
