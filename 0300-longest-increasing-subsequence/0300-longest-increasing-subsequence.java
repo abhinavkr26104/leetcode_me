e9167bcb3cc0b16f2a25411c7bc491ef57c7c5e3
Abhinav Kumar Singh <152812718+abhinavkr26104@users.noreply.github.com>
2026-05-16 20:10:45 +0530

    Time: 2 ms (100%), Space: 45.9 MB (91.34%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,20 +1,44 @@
-import java.util.*;
-
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int[] tails = new int[nums.length];
-        int size = 0;
+        int temp[]=new int[nums.length+1];
+        int size=0;
 
-        for (int x : nums) {
-            int i = Arrays.binarySearch(tails, 0, size, x);
-
-            if (i < 0) i = -(i + 1); // insertion point
-
-            tails[i] = x;
-
-            if (i == size) size++;
+        for(int i:nums)
+        {
+             if (size == 0) {
+                temp[++size] = i;
+                continue;
+            }
+            int x=temp[size];
+            if(i>x)
+            temp[++size]=i;
+            else
+            {
+                int p=lowerBound(temp,i,size);
+                temp[p]=i;
+            }
         }
 
         return size;
+
+        
+    }
+     public static int lowerBound(int[] arr, int x,int size) {
+        int low = 1;
+        int high = size;
+        int ans = size;  
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] >= x) {
+                ans = mid;      
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return ans;
     }
 }
