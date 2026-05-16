7fd43f6e2e2eb905582fcff63a1c62c3511f1514
Abhinav Kumar Singh <152812718+abhinavkr26104@users.noreply.github.com>
2026-05-16 22:01:14 +0530

    Time: 19 ms (73.8%), Space: 46.6 MB (23.7%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,38 +1,50 @@
+import java.util.ArrayList;
+
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int temp[]=new int[nums.length+1];
-        int size=0;
+        int temp[] = new int[nums.length + 1];
+        int size = 0;
 
-        for(int i:nums)
-        {
-             if (size == 0) {
+        for (int i : nums) {
+            if (size == 0) {
                 temp[++size] = i;
                 continue;
             }
-            int x=temp[size];
-            if(i>x)
-            temp[++size]=i;
-            else
-            {
-                int p=lowerBound(temp,i,size);
-                temp[p]=i;
+
+            int x = temp[size];
+
+            if (i > x)
+                temp[++size] = i;
+            else {
+                int p = lowerBound(temp, i, size);
+                temp[p] = i;
             }
         }
 
-        return size;
+        ArrayList<Integer> ans = new ArrayList<>();
 
-        
+        for (int i = 1; i <= size; i++) {
+            ans.add(temp[i]);
+        }
+
+        for (int i : ans) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        return size;
     }
-     public static int lowerBound(int[] arr, int x,int size) {
+
+    public static int lowerBound(int[] arr, int x, int size) {
         int low = 1;
         int high = size;
-        int ans = size;  
+        int ans = size;
 
         while (low <= high) {
             int mid = low + (high - low) / 2;
 
             if (arr[mid] >= x) {
-                ans = mid;      
+                ans = mid;
                 high = mid - 1;
             } else {
                 low = mid + 1;
