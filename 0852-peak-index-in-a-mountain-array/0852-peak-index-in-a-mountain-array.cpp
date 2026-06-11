7c32f810bb8ab92887bef23829782dff9db410c3
Abhinav Kumar Singh <152812718+abhinavkr26104@users.noreply.github.com>
2026-06-11 18:03:27 +0530

    Time: 0 ms (100%), Space: 63.7 MB (10.66%) - LeetHub

---
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& nums) {
+        int n = nums.size();
+
+        int x = -1;
+
+        int z = 1;
+        while (z < n) z *= 2;
+
+        for (int b = z; b >= 1; b /= 2) {
+            while (x + b + 1 < n && nums[x + b] < nums[x + b + 1]) {
+                x += b;
+            }
+        }
+
+        return x + 1;
+    }
+};
