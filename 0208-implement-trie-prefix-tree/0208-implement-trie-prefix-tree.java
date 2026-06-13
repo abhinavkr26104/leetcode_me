3c618533cc4008bb853ee2ff75b95bdc69422270
Abhinav Kumar Singh <152812718+abhinavkr26104@users.noreply.github.com>
2026-06-13 23:53:12 +0530

    Time: 32 ms (77.12%), Space: 61.9 MB (73.08%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
@@ -1,23 +1,54 @@
 class Trie {
 
-    Set<String> set = new HashSet<>();
-
-    public Trie() {
+    class Node {
+        Node[] child = new Node[26];
+        boolean end;
     }
 
+    Node root = new Node();
+
     public void insert(String word) {
-        set.add(word);
+        Node cur = root;
+
+        for (char ch : word.toCharArray()) {
+            int c = ch - 'a';
+
+            if (cur.child[c] == null)
+                cur.child[c] = new Node();
+
+            cur = cur.child[c];
+        }
+
+        cur.end = true;
     }
 
     public boolean search(String word) {
-        return set.contains(word);
+        Node cur = root;
+
+        for (char ch : word.toCharArray()) {
+            int c = ch - 'a';
+
+            if (cur.child[c] == null)
+                return false;
+
+            cur = cur.child[c];
+        }
+
+        return cur.end;
     }
 
     public boolean startsWith(String prefix) {
-        for (String s : set) {
-            if (s.startsWith(prefix))
-                return true;
+        Node cur = root;
+
+        for (char ch : prefix.toCharArray()) {
+            int c = ch - 'a';
+
+            if (cur.child[c] == null)
+                return false;
+
+            cur = cur.child[c];
         }
-        return false;
+
+        return true;
     }
 }
