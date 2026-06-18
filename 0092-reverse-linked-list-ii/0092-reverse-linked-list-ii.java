0bfd94b13c764f7a3193c2d0d2b5ee35a640cffc
Abhinav Kumar Singh <152812718+abhinavkr26104@users.noreply.github.com>
2026-06-18 10:28:09 +0530

    Time: 0 ms (100%), Space: 43 MB (65.53%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
@@ -9,13 +9,11 @@ class Solution {
 
         ListNode prev = dummy;
 
-        // reach node before left
         for (int i = 1; i < left; i++)
             prev = prev.next;
 
         ListNode cur = prev.next;
 
-        // reverse right-left+1 nodes
         for (int i = 0; i < right - left; i++) {
 
             ListNode next = cur.next;
