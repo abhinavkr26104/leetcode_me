29399a767f17f90ad0481337b1f842ff24848460
Abhinav Kumar Singh <152812718+abhinavkr26104@users.noreply.github.com>
2026-06-24 10:09:10 +0530

    Time: 0 ms (100%), Space: 42.1 MB (32.86%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,3 +1,16 @@
+// class Solution {
+//     public int fib(int n) {
+//         if(n==0 || n==1)
+//         {
+//             return n;
+//         }
+//         else
+//         {
+//             return fib(n-1)+fib(n-2);
+//         }
+//     }
+// }
+
 class Solution {
     public int fib(int n) {
         int p1=0;
