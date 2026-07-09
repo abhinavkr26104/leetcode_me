80127fd00292586c0bbd066394a90879e126b4c9
Abhinav Kumar Singh <152812718+abhinavkr26104@users.noreply.github.com>
2026-07-09 21:26:41 +0530

    Time: 31 ms (25.52%), Space: 48.7 MB (79.18%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.java
+++ 0085-maximal-rectangle/0085-maximal-rectangle.java
@@ -1,38 +1,73 @@
 class Solution {
-    public int maximalRectangle(char[][] matrix) {
-        if (matrix.length == 0) return 0;
 
-        int cols = matrix[0].length;
-        int[] height = new int[cols];
+    public int maximalRectangle(char[][] matrix) {
+
+        if (matrix.length == 0)
+            return 0;
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int[] heights = new int[n];
         int maxArea = 0;
 
-        for (char[] row : matrix) {
-            for (int j = 0; j < cols; j++) {
-                if (row[j] == '1')
-                    height[j]++;
+        for (int i = 0; i < m; i++) {
+
+            for (int j = 0; j < n; j++) {
+
+                if (matrix[i][j] == '1')
+                    heights[j]++;
                 else
-                    height[j] = 0;
+                    heights[j] = 0;
             }
-            maxArea = Math.max(maxArea, largestRectangleArea(height));
+
+            maxArea = Math.max(maxArea, largestRectangleArea(heights));
         }
 
         return maxArea;
     }
 
     private int largestRectangleArea(int[] heights) {
+
+        int n = heights.length;
+        int[] pse = new int[n];
+        int[] nse = new int[n];
+
         Stack<Integer> st = new Stack<>();
-        int maxArea = 0;
 
-        for (int i = 0; i <= heights.length; i++) {
-            int h = (i == heights.length) ? 0 : heights[i];
+        // Previous Smaller Element
+        for (int i = 0; i < n; i++) {
 
-            while (!st.isEmpty() && h < heights[st.peek()]) {
-                int height = heights[st.pop()];
-                int width = st.isEmpty() ? i : i - st.peek() - 1;
-                maxArea = Math.max(maxArea, height * width);
-            }
+            while (!st.isEmpty() && heights[st.peek()] >= heights[i])
+                st.pop();
+
+            pse[i] = st.isEmpty() ? -1 : st.peek();
+
             st.push(i);
         }
+
+        st.clear();
+
+        // Next Smaller Element
+        for (int i = n - 1; i >= 0; i--) {
+
+            while (!st.isEmpty() && heights[st.peek()] >= heights[i])
+                st.pop();
+
+            nse[i] = st.isEmpty() ? n : st.peek();
+
+            st.push(i);
+        }
+
+        int maxArea = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            int width = nse[i] - pse[i] - 1;
+
+            maxArea = Math.max(maxArea, heights[i] * width);
+        }
+
         return maxArea;
     }
 }
